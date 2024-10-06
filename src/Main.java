911979bbfe7c9c90058d1bca2922efe30a74ad0a
Дмитрий Захаров <zds.vip1221@gmail.com>
2024-10-06 16:06:49 +0400

    tasks solutions

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,52 @@
 public class Main {
     public static void main(String[] args) {
         //task1
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        //task2
+        for (int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
+
+        //task3
+        //0 является четным числом
+        for (int i = 0; i <= 17; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+        //task4
+        for (int i = 10; i >= -10; i--){
+            System.out.println(i);
+        }
+
+        //task5
+
+        for (int i = 1904; i <= 2096; i += 4){
+            System.out.println(i + " год является високосным");
+        }
+
+        //task6
+
+        for (int i = 7; i <= 98; i += 7){
+            System.out.println(i);
+        }
+        //task7
+
+        int myYearSavings = 0;
+
+        for (int i = 1; i <= 12; i++){
+            myYearSavings = myYearSavings + (myYearSavings / 100) + 29000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + myYearSavings + " рублей");
+        }
+
+        //task8
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println("2 * " + i + " = " + (2 * i));
+        }
     }
 }
